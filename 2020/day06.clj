2316bd2ee314aca990060b56ae73a5dd77961bae
Todd Smith <tmsmith328@gmail.com>
2021-09-25 15:22:03 -0600

    day 6 solution

--- 2020/day06.clj
+++ 2020/day06.clj
@@ -1,11 +1,13 @@
-(ns aoc2020.core
+;; Advent of Code 2020
+;; Day 6
+;;
+;; https://adventofcode.com/2020/day/6
+
+(ns user
   (:gen-class)
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
             [clojure.set :as set]))
-
-;; Advent of Code 2020
-;; Day 6
 
 ;; Sample Data
 (def sample "abc
@@ -28,6 +30,7 @@ b")
   "swaps string/split parameters of idiomatic ->> threading"
   (str/split s regex))
 
+(def input-file "day06-input.txt")
 
 (defn count-any-yes [serialized]
   (-> serialized
@@ -35,7 +38,8 @@ b")
       (disj \newline)
       count))
 
-(count-any-yes "abcx\nabcy\nabcz")
+;; testing the count-any-yes function
+(count-any-yes "abcx\nabcy\nabcz")  ;; => 6
 
 
 (defn count-all-yes [serialized]
@@ -46,33 +50,37 @@ b")
        count
        ))
 
+;; testing the count-all-yes function
+(count-all-yes "abcx\nabcy\nabcz")  ;; => 3
+
+
 ;; Part 1 - Count any yes
-(->> "/home/todd/dev/clojure/aoc2020/puzzlefiles/day6.txt"
-     slurp
-     (split #"\n\n")
-     (map count-any-yes)
-     (reduce +))   ;672
+(defn part1 []
+  (->> input-file
+       slurp
+       (split #"\n\n")
+       (map count-any-yes)
+       (reduce +)))   ;672
 
 ;; Part 2 - Count all yes
-(->> "/home/todd/dev/clojure/aoc2020/puzzlefiles/day6.txt"
-     slurp
-     (split #"\n\n")
-     (map count-all-yes)
-     (reduce +))   ;3268
+(defn part2 []
+  (->> input-file
+       slurp
+       (split #"\n\n")
+       (map count-all-yes)
+       (reduce +)))   ;3268
+
+(part1)    ;; => 6612
+(part2)    ;; => 3268
 
 
-
-
+;; ========================================================
 ;; Test code
-(count-all-yes "abcx\nabcy\nabcz")
 
 (split #"\n\n" sample)     ; ["abc" "a\nb\nc" "ab\nac" "a\na\na\na" "b"]
-
-(split #"\n\n" sample)
 
 (-> sample
     set
     (disj \newline)
     count)
 
-(+ 1 1)
