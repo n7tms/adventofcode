65cd55a87ebfbadeb57f77c58c963b3d70cf05ec
Todd Smith <n7tms@yahoo.com>
2021-10-08 09:45:32 -0600

    day08 (fo) solution

--- 2020/day08-fo.clj
+++ 2020/day08-fo.clj
@@ -9,7 +9,8 @@
 ;; with some adaptations to my style and preferences.
 
 
-(def input-file "day08-input.txt")
+;(def input-file "day08-input.txt")
+(def input-file "src/working/day08-input.txt")
 
 :input "acc -2"
 ;; Receives a line of code from the input and converts it to a 
@@ -36,13 +37,15 @@
          idx 0
          visited #{}]
     (if (contains? visited idx)
-      accum
-      (let [[op arg] (program idx)]
-        (case op
-          :acc (recur (+ accum arg) (inc idx)   (conj visited idx))
-          :jmp (recur accum         (+ idx arg) (conj visited idx))
-          :nop (recur accum         (inc idx)   (conj visited idx))
-          ))))
+      {:status :loops, :acc accum}
+      (if (= idx (count program))
+        {:status :halts, :acc accum}
+        (let [[op arg] (program idx)]
+          (case op
+            :acc (recur (+ accum arg) (inc idx)   (conj visited idx))
+            :jmp (recur accum         (+ idx arg) (conj visited idx))
+            :nop (recur accum         (inc idx)   (conj visited idx))
+            )))))
   )
 
 ;; this block is used for testing the parsing and logic.
@@ -56,21 +59,42 @@ jmp -5"
      (mapv parse-instr)
      run)
 
+(def the-code
+  (->> (slurp input-file)
+      str/split-lines
+      (mapv parse-instr)))
+
+
 ;; Using the input-file, split the lines and then parse each
 ;; of them into a vector. Then throw that entire vector at
 ;; the part1 run, above.
 (defn part1 []
-  (->> (slurp input-file)
-       str/split-lines
-       (mapv parse-instr)
+  (->> the-code
        run
+       :acc
        ))
 (part1)  ;; => 1134
 
 
+(defn part2 []
+  (->> the-code
+       count
+       range
+       (map #(update-in the-code [% 0]
+               {:jmp :nop
+                :nop :jmp
+                :acc :acc} ))
+       (map run)
+       (filter #(= :halts (:status %)))
+       first
+       :acc
+  )
+)
 
-;; I'm currently at 14:28 in the video. (part2)
+(part2)   ;; => 1205
 
 
+
+;; Fred is using the following pattern to change the jmps to nops and the nops to jmps
 (def odds [[1 3] [5 7]])
 (update-in odds [1 0] inc)
