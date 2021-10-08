c2c2ca8845a96d0a075716d21ba422973fa9d26b
Todd Smith <n7tms@yahoo.com>
2021-10-08 12:26:34 -0600

    day09 solution

--- 2020/day09.clj
+++ 2020/day09.clj
@@ -4,7 +4,8 @@
 ;; Advent of Code 
 ;; Day 09
 ;;
-;;
+;;  .... with help from Fred Overflow
+;;  .... https://www.youtube.com/watch?v=61ksBHphf4s&list=PLbPrugU2oQ8VURsQdZ6W_iovXRS24UmZQ&index=9
 
 
 (def input-file "src/working/day09-input.txt")
@@ -33,27 +34,74 @@
 
 (defn parsed [x]
 ;  (Integer/parseInt x)
-  (bigint x)
+  (Long/parseLong x)
   )
 
-;; (def the-numbers
-;;   (->> (slurp input-file)
-;;       str/split-lines
-;;       (map parsed)
-;;       ))
-
 (def the-numbers
-  (->> sample
-       str/split-lines
-       (map parsed)))
+  (->> (slurp input-file)
+      str/split-lines
+      (mapv parsed)
+      ))
+
+;; (def the-numbers
+;;   (->> sample
+;;        str/split-lines
+;;        (mapv parsed)))
 
 (take 5 (drop 1 the-numbers))
 
-(defn sum [x]
+(defn sum? [x]
   (let [addends (take 5 (drop (- x 5) the-numbers))]
     addends)
   )
 (nth the-numbers 5)
 
 (sum? 6)
-(reduce (+ 64) sum)
+the-numbers
+
+(defn sum-pair [numbers target]
+  (let [n (count numbers)]
+    (first  (for [i (range (dec n))
+                  j (range (inc i) n)
+                  :when (= target (+ (numbers i) (numbers j)))]  [i j]))))
+
+
+(sum-pair (subvec the-numbers 1 6) 62)
+
+(defn invalid [n numbers]
+  (first (for [i (range (- (count numbers) n))
+               :let [k (+ i n)
+                     window (subvec numbers i k)
+                     target (numbers k)]
+               :when (not (sum-pair window target))]
+           target)))
+
+(defn part1 []
+  (invalid 25 the-numbers))
+
+(part1)   ;; => 26796446
+
+
+
+(defn summands [n numbers]
+  (loop [i 0
+         k 0
+         s (- (invalid n numbers))]
+    (cond
+      (neg? s) (recur i (inc k) (+ s (numbers k)))
+      (pos? s) (recur (inc i) k (- s (numbers i)))
+      :otherwise
+      (sort (subvec numbers i k)))
+    ))
+
+
+(defn part2 [n numbers]
+  (let [a (first (summands n numbers))
+        b (last (summands n numbers))]
+    (+ a b)))
+
+
+
+(part2 25 the-numbers)  ;; => 3353494
+
+
