b3e2c8aacd38b9b2a3bb73eddec0cccfb1f6e7e0
Todd Smith <n7tms@yahoo.com>
2021-10-21 17:33:37 -0600

    day15 p1 solution

--- 2020/day15.clj
+++ 2020/day15.clj
@@ -2,27 +2,37 @@
 
 
 (def small "0,3,6")                ;; => 436
+(def small3 "0,3,6,0,3,3,1")                ;; => 436
 (def small2 "0,3,6,0,3,3,1,0,4,0")
 (def large "0,13,16,17,1,10,6")
 
+(def start-seq (mapv #(Integer/parseInt %) (re-seq #"\d+" large)))
+
+(defn in? [coll elm]
+  (some #(= elm %) coll))
+
+(defn next-number [x]
+  (if (in? (butlast x) (last x))
+    (- (count x) (inc (last (keep-indexed #(if (= (last x) %2) %1) (butlast x)))))
+    0)
+)
+
+;; (def b [0 3 6 0 3 3 1 0 4 0])
+;; (conj b (next-number b))
+;; (if (zero? (inc (.lastIndexOf (butlast b) (int (last b)))))
+;;   0
+;;   (- (count b) (inc (.lastIndexOf (butlast b) (int (last b))))))
+
+;; (next-number b)
+
+(defn part1 [begin]
+  (loop [x begin]
+    (if (= 2020 (count x))
+      (last x)
+      (recur (conj x (next-number x))))))
+
+(part1 start-seq)  ;; => 276
 
 
-(defn index-of-last [num so-far]
-  (inc (.lastIndexOf so-far (int num)))
-  )
 
-
-(def so-far (map #(Integer/parseInt %) (re-seq #"\d+" small2)))
-
-(index-of-last 6 so-far)
-
-
-(last so-far)
-
-(concat so-far (fn (if (zero? (index-of-last (last so-far) (butlast so-far)))
-                      0
-                      (- (count so-far) (index-of-last (last so-far) (butlast so-far))))))
-
-;(concat  so-far '(0))
-;(index-of-last 0 (butlast so-far))
 
