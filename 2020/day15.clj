c25f32355830dab060bac3cdb73473cb1867f9d6
Todd Smith <n7tms@yahoo.com>
2021-10-22 10:33:03 -0600

    day15 solution

--- 2020/day15.clj
+++ 2020/day15.clj
@@ -1,3 +1,11 @@
+;; Advent Of Code
+;; Day 15 - Rambunctious Recitation
+;;
+;; Part 1 is all me.
+;; My part 2 solution was going to take about 15 days play out. So, 
+;; I borrowed Fred Overflow's part 2 solution.
+;;
+
 (ns adventofcode)
 
 
@@ -24,12 +32,27 @@
       (last x)
       (recur (conj x (next-number x))))))
 
-(time (part1 start-seq))
+;(time (part1 start-seq))
 ;; => 276 in 93 sec
 ;; by my calculations, it will take 15 days to crunch through to a solution for part 2.
 
 
+(defn starting [^String input]
+  (->> input
+       (re-seq #"\d+")
+       (map-indexed (fn [^long index, ^String number]
+                      [(Integer/parseInt number) (inc index)]))))
 
+(defn part2 [the-input]
+  (loop [spoken (into {} (starting the-input))
+         number 0
+         turn (inc (count spoken))]
+    (if (= 30000000 turn)
+      number
+      (recur
+       (assoc spoken number turn)
+       (- turn (get spoken number turn))
+       (inc turn)
+       ))))
 
-
-
+(part2 large)  ;; => 31916 in 44 seconds.
