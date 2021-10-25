844ef0050bd7d0b47e924faf00a5954578d83f64
Todd Smith <tmsmith328@gmail.com>
2021-10-25 12:41:26 -0600

    day18 solution

--- 2020/day18.clj
+++ 2020/day18.clj
@@ -1,8 +1,13 @@
-;; Advent of Code
+1048576;; Advent of Code
 ;; Day 18
 
 
-(ns adventofcode)
+(ns adventofcode
+  (:require [clojure.string :as str]))
+
+(defn split [regex s]
+  (str/split s regex))
+
 
 (def small1 "1 + 2 * 3 + 4 * 5 + 6")                            ;; => 71
 (def small2 "1 + (2 * 3) + (4 * (5 + 6))")                      ;; => 51
@@ -11,4 +16,101 @@
 (def small6 "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")        ;; => 12240
 (def small5 "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")  ;; => 13632
 
+(def large (slurp "day18-input.txt"))
 
+
+
+;; I thought this was going to be a straight forward implementation of a "compiler",
+;; but I ended up getting bogged down in the clojure constructs again. So, again,
+;; I turned to Fred ... and his solution was WAY over my head. I'm not sure I would
+;; have ever gotten here using clojure!!!
+
+;; expression = element { ( *|+ ) } element
+;; element    = number | ( expression )
+
+(defn evaluate [^String input]
+  (let [lexemes (volatile! (re-seq #"\d|[+*()]" input))]
+    (letfn [(current [] (first (deref lexemes)))
+
+            (accept [] (vswap! lexemes rest))
+
+            (expression []
+              (loop [temp (primary)]
+                (case (current)
+                  "*" (do
+                        (accept)
+                        (recur (* temp (primary))))
+                  "+" (do
+                        (accept)
+                        (recur (+ temp (primary))))
+                  temp)))
+
+            (primary []
+              (case (current)
+                "(" (do
+                      (accept)
+                      (let [temp (expression)]
+                        (accept) ;; assume closing paren
+                        temp))
+                (let [temp (Integer/parseInt (current))]
+                  (accept)
+                  temp)))]
+      (expression))))
+
+
+(defn part1 [input]
+  (->> input
+       string/split-lines
+       (map evaluate)
+       (reduce +)))
+
+(part1 large)   ;; => 4491283311856
+
+;; Part 2 ==========================================
+
+;; product  = sum { '*' sum }
+;; sum      = primary { '+' primary }
+;; primary  = number | '(' product ')
+
+
+(defn evaluate2 [^String input]
+  (let [lexemes (volatile! (re-seq #"\d|[+*()]" input))]
+    (letfn [(current [] (first (deref lexemes)))
+
+            (accept [] (vswap! lexemes rest))
+
+            (product []
+              (loop [temp (sum)]
+                (case (current)
+                  "*" (do
+                        (accept)
+                        (recur (* temp (sum))))
+                  temp)))
+
+            (sum []
+              (loop [temp (primary)]
+                (case (current)
+                  "+" (do
+                        (accept)
+                        (recur (+ temp (primary))))
+                  temp)))
+
+            (primary []
+              (case (current)
+                "(" (do
+                      (accept)
+                      (let [temp (product)]
+                        (accept) ;; assume closing paren
+                        temp))
+                (let [temp (Integer/parseInt (current))]
+                  (accept)
+                  temp)))]
+      (product))))
+
+(defn part2 [input]
+       (->> input
+            string/split-lines
+            (map evaluate2)
+            (reduce +)))
+
+(part2 large)  ;; => 68852578641904
---
+++ 2020/day19.clj
@@ -0,0 +1,11 @@
+;; Advent of Code
+;; Day 19 - 
+;;
+
+(ns adventofcode)
+
+(def small (slurp "day19-smallinput.txt"))
+
+(def large (slurp "day19-input.txt"))
+
+
