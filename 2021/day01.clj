5082480b8a4530920c93ef6c3b63c18938f5450b
Todd Smith <tmsmith328@gmail.com>
2021-12-01 13:01:03 -0700

    2021.1 solution

---
+++ 2021/day01.clj
@@ -0,0 +1,49 @@
+;; Advent of Code
+;; Day 01 - Sonar Sweep
+;;
+(ns aoc
+  (:require [clojure.string :as str]))
+
+(def large (slurp "day01-input.txt"))
+
+(def small "199
+200
+208
+210
+200
+207
+240
+269
+260
+263")
+
+;; Parse the input by splitting it into separate lines and
+;; converting each term to an integer.
+(def parsed 
+  (->> large
+       (str/split-lines)
+       (map #(Integer/parseInt %))))
+
+
+;; Partition the input (eg. (199 200 208 210 ... ) => ((199 200) (200 208) (208 210) ...) )
+;; then iterate through each of the pairs comparing. Return a 1 if last if greater than first.
+;; Otherwise a 0. Add up all the 1's. That how many depth increases exists.
+(defn part1 []
+  (apply +
+         (for [x (partition 2 1 parsed)]
+           (if (> (last x) (first x)) 1 0)
+           )))
+
+(part1)  ;; => 1696
+
+
+;; Same as part 1, only instead of comparing initial pairs, we are going to compare running
+;; sums of 3. (eg. ((199 200 208 210 ... ) => ((199 200 208) (200 208 210)) => ((607 618) ...)
+(defn part2 []
+  (apply + 
+         (for [x (partition 2 1  ;; partition running sums into groups of 2
+                            (for [x (partition 3 1 parsed)] ;; sum of groups of three
+                              (apply + x)))]
+           (if (> (last x) (first x)) 1 0))))
+
+(part2)   ;; => 1737
