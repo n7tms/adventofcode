69506c06c0a044e2014157dbf065a77396c1fc63
Todd Smith <tmsmith328@gmail.com>
2021-12-11 18:19:02 -0700

    working day 11

--- 2021/day11.clj
+++ 2021/day11.clj
@@ -16,8 +16,18 @@
 4846848554
 5283751526")
 
+(def vs "6594254334
+3857965822")
+
+(def sample "11111
+19991
+19991
+19991
+11111")
+
 (def large (slurp input-file))
 
+;(def active sample)
 (def active small)
 ;(def active large)
 
@@ -35,13 +45,12 @@
       (map #(Integer/parseInt %) o)))))
 
 
-;; (def width (count (first energy)))
-;; (def height (count energy))
 (def width 10)
 (def height 10)
 
+
 ;; directions like on a clock: 1:00, 3:00, 5:00, 6:00 etc.
-(def directions [(inc (- width)) 1 (inc width) width (dec width) -1 (dec (- width)) -10])
+(def directions [(inc (- width)) 1 (inc width) width (dec width) -1 (dec (- width)) (- width)])
 
 (defn increase-energy [energy]
   (mapv inc energy))
@@ -58,53 +67,80 @@
                  )]
      (if (zero? (mod pos width))
        ;; remove positions beyond the left edge
-       (remove #(= (+ pos 9) %)
-               (remove #(= (- pos 11) %) 
+       (remove #(= (+ pos (dec width)) %)
+               (remove #(= (- pos (inc width)) %) 
                        (remove #(= (dec pos) %) d1)))
        ;; remove positions beyond the right edge
-       (if (= 9 (mod pos 10))
-         (remove #(= (- pos 9) %)
+       (if (= 9 (mod pos width))
+         (remove #(= (- pos (dec width)) %)
                  (remove #(= (inc pos) %)
-                         (remove #(= (+ pos 11) %) d1)))
+                         (remove #(= (+ pos (inc width)) %) d1)))
          d1)))))
 
+(defn return-to-z [engs valid-targets]
+  (loop [e engs
+         vt valid-targets]
+    (if (empty? vt) e
+        (recur (assoc e (first vt) 0) (rest vt)))))
+
+
+
+;; How to deal with a 10 that gets inc'd to 11, before it gets to flash it's neighbors?
+(defn flashable [e]
+  (remove nil?
+          (for [x (range (count e))]
+            (if (>= 10 (nth e x)) x))))
+
+
+(defn flash-pos [pos energies]
+  (loop [vt (valid-targets pos)
+         e (return-to-z energies vt)]
+    (let [fvt (first vt)]
+      (if (empty? vt) e
+          (if (= 0 (nth e fvt))
+            (recur (rest vt) e)
+            (recur (rest vt) (assoc e fvt (inc (nth e fvt)))))
+)
+)))
+
+
+
+(defn flash [e]
+  (loop [e1 e]
+    (if (> (count (filter #(> % 9) e1)) 0)
+      (recur
+       (loop [f (flashable e1)
+              new-e e1]
+         (if (empty? f)
+           new-e
+           (recur (rest f) (flash-pos (first f) new-e)))
+         ))
+      e1))
+)
 
 
 
 
-(defn flash-oct [pos energy]
-  (for [i (range (* width height))]
-    (let [e (nth energies i) []]
-      (if (=> e 10)
-        (assoc energy i 0 ))))
-
+(defn part1 [goal]
+  (loop [steps   0
+         flashes 0
+         e       energies]
+    (if (>= steps goal)
+      (do (println e)
+          flashes)
+      (let [oct (->> e
+                     (increase-energy)
+                     (flash)
+                     )]
+        (recur (inc steps)
+               (+ flashes (count (filter #(= % 0) oct))) ;flashes
+               oct)
+        )
+      )
+    )
   )
 
-;; continue working on flash-oct
-;; keep everything working on vectors so we can use assoc to change energies
-;; flash-oct will change the pos from 10 to 0 and inc the energies around it
-;;   that are not already 0.
-
-;; pseudocode
-;; increment all the energies
-;; iterate through looking for any 10's
-;; . change the 10 to a 0
-;; . inc any energies around this position that are not already 0
-;; repeat this a specified (100) number of times.
-;; 
-;; read the problem statement again. We are supposed to be counting flashes
-;; along the way. But I think it is more than just counting 0's in each 
-;; step/iteration/phase.
-;; 
-
-;;1:45 so far.
-
-
-
-
-(defn part1 []
-
-  )
+(part1 1)
 
 
 
@@ -114,12 +150,13 @@
   )
 
 
-(println "Part1: " (part1) "\nPart2: " (part2))
+;(println "Part1: " (part1) "\nPart2: " (part2))
 
 
 
 (deftest sample-data-test
-  (clojure.test/is (= (part1) 1656))
+  (clojure.test/is (= (part1 100) 1656))
+  (clojure.test/is (= (part1 10) 204))
   (clojure.test/is (= (part2) 00)))
 
 (run-tests)
