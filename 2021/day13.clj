7139c33eeb3417a720cc6ee1fad180edfa54369c
Todd Smith <tmsmith328@gmail.com>
2021-12-13 10:17:39 -0700

    day13 solution

--- 2021/day13.clj
+++ 2021/day13.clj
@@ -1,7 +1,7 @@
 (ns aoc2021
   (:require [clojure.string :as string]))
 
-;; TODO: change 00 to current day number
+
 (def day "13")
 (def input-file (str "day" day "-input.txt"))
 
@@ -22,56 +22,50 @@
 1,10
 2,14
 8,10
-9,0")
-(def small-fold "fold along y=7
+9,0
+
+fold along y=7
 fold along x=5")
 
 (def large (slurp input-file))
-(def large-fold "fold along x=655
-fold along y=447
-fold along x=327
-fold along y=223
-fold along x=163
-fold along y=111
-fold along x=81
-fold along y=55
-fold along x=40
-fold along y=27
-fold along y=13
-fold along y=6")
-
-(def instr '(["x" 655] ["y" 447] ["x" 327] ["y" 223] ["x" 163] ["y" 111] ["x" 81] ["y" 55] ["x" 40] ["y" 27] ["y" 13] ["y" 6]))
-
-;(def instr '(["y" 7] ["x" 5]))
-
 
 ;(def active small)
 (def active large)
 
-(defn split [regex s]
-  (string/split s regex))
 
-
-(defn parse-line [line]
-  (let [[_              x    y]
-        (re-matches  #"(\d+),(\d+)" line)]
+;; ============================================================
+;;              PARSE INPUT
+;; ============================================================
+(defn parse-c [c]
+  (let [[_               x     y]
+        (re-matches  #"(\d+),(\d+)" c)]
     [(Integer/parseInt x) (Integer/parseInt y)]))
 
+(defn parse-f [f]
+  (let [[_                        dir   mag]
+        (re-matches #"fold along (x|y)=(\d+)" f)]
+    [dir (Integer/parseInt mag)]))
 
-(def coords
-  (->> (string/split-lines active)
-       (map #(parse-line %))
+
+(defn parse-coords [coords]
+  (->> (string/split-lines coords)
+       (map #(parse-c %))
        ))
 
+(defn parse-folds [folds]
+  (->> (string/split-lines folds)
+       (map #(parse-f %))))
+
+(def input
+  (let [[coords folds] (string/split active #"\n *\n")]
+    {:coords (parse-coords coords)
+     :folds (parse-folds folds)}
+    ))
 
 
-(defn max-y [matrix]
-  (apply max (for [[x y] matrix] y)))
-
-(defn max-x [matrix]
-  (apply max (for [[x y] matrix] x)))
-
-
+;; ============================================================
+;;              FOLD FUNCTIONS
+;; ============================================================
 (defn fold-y [coords y-line]
   (for [points coords]
     (let [[x y] points]
@@ -87,48 +81,37 @@ fold along y=6")
         [x y]))))
 
 
-(def plot-points 
-  (loop [i instr
+(defn execute-folds [coords folds]
+  (loop [f folds
          c coords]
-    (if (empty? i) c
-        (let [[dir mag] (first i)]
+    (if (empty? f) c
+        (let [[dir mag] (first f)]
           (cond
-              (= dir "x") (recur (rest i) (fold-x c mag))
-              (= dir "y") (recur (rest i) (fold-y c mag)))))))
+              (= dir "x") (recur (rest f) (fold-x c mag))
+              (= dir "y") (recur (rest f) (fold-y c mag)))))))
 
 
-
-
-(spit "day13-output.txt" (string/join " " (for [[x y] plot-points]
-                                            (str "(" x "," y ")"))))
-
-
-
- ;; between 808 and 922
-
-
-
+;; ============================================================
+;;              SOLUTIONS
+;; ============================================================
 
 (defn part1 []
-
+"Count the number of visible dots after one fold"
+  (count
+   (distinct
+    (execute-folds (:coords input) (list (first (:folds input))))))
+  
   )
-
-
-
 
 (defn part2 []
-
+"after the folds, what eight letters appear on the paper"
+  (use '(incanter core stats charts))
+  (let [x (for [[x y] (execute-folds (:coords input) (:folds input))] x)
+        y (for [[x y] (execute-folds (:coords input) (:folds input))] y)]
+    (view (scatter-plot x y))
+    )
   )
-
-
 
 (println "Part1: " (part1) "\nPart2: " (part2))
 
-
-(use 'clojure.test)
-(deftest sample-data-test
-  (clojure.test/is (= (part1) 17))
-  (clojure.test/is (= (part2) 00)))
-
-(run-tests)
 
