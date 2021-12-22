a9892079208a5109852370efdfa58dcc37bba838
Todd Smith <tmsmith328@gmail.com>
2021-12-21 19:05:28 -0700

    day21 solution

--- 2021/day21.clj
+++ 2021/day21.clj
@@ -9,8 +9,8 @@
 (def small [4 8])
 (def large [8 1])
 
-(def active small)
-;(def active large)
+;(def active small)
+(def active large)
 
 
 
@@ -39,13 +39,32 @@
                 (recur (+ dice 3) (+ rolls 3) pos1 p2 score1 s2 1)))))))
 
 
+;; Part 2 solution from 
+;; https://github.com/wevre/advent-of-code/blob/master/src/advent_of_code/2021/day_21.clj
+;; (def p1 8)
+;; (def p2 1)
+
+(defn wrap-10 [x] (inc (mod (dec x) 10)))
+
+(def quantum
+  (memoize
+   (fn
+     ([p1 p2] (quantum p1 p2 0 0))
+     ([p1 p2 sc1 sc2]
+      (if (<= 21 sc2)
+        [0 1]
+        (reduce #(mapv + %1 %2)
+                (for [r1 [1 2 3] r2 [1 2 3] r3 [1 2 3]
+                      :let [p1 (wrap-10 (+ p1 r1 r2 r3))
+                            sc1 (+ sc1 p1)]]
+                  (reverse (quantum p2 p1 sc2 sc1)))))))))
 
 (defn part2 []
+ (apply max (quantum (first active) (last active))))
 
-  )
 
 ;(part1)   ;; => 518418
-
+;(part2)   ;; => 116741133558209
 
 (println "Part1: " (part1) "\nPart2: " (part2))
 
@@ -53,7 +72,12 @@
 (use 'clojure.test)
 (deftest sample-data-test
   (test/is (= (part1) 739785))
-  (test/is (= (part2) 0)))
+  (test/is (= (part2) 444356092776315)))
 
 (run-tests)
 
+
+
+;; This part2 solution...
+;; https://github.com/pbruyninckx/aoc2021/blob/main/src/aoc/day21_2.clj
+;; ... looks a little more like my style ... lots of functions, etc.
