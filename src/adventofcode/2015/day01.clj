c0ac614901a532c180fa945221cd16c0dc0ef9f0
Todd Smith <n7tms@yahoo.com>
2021-12-29 16:07:46 -0700

    modified 2015-01

--- src/adventofcode/2015/day01.clj
+++ src/adventofcode/2015/day01.clj
@@ -1,18 +1,18 @@
 ;; Advent of Code 2015
 ;; Day 01 - Not Quite Lisp
 ;;
-;; 
+;;
 ;; ( ~ up
 ;; ) ~ down
 ;;
 ;; Just count the number of "ups" (+) and "downs" (-) and you should get the final floor.
 ;;
 
-(def large (slurp "2015-day01-input.txt" ))
-(def small "(()))(((") 
+(def large (slurp "src/adventofcode/2015/2015-day01-input.txt" ))
+(def small "(()))(((")
 
 (defn part1 []
-  (- 
+  (-
    (count (re-seq #"\(" large))
    (count (re-seq #"\)" large))))
 
@@ -21,16 +21,19 @@
 
 ;; After the fact, I found the following "cool" code block at
 ;; https://github.com/AxlLind/AdventOfCode2015/blob/main/src/01.clj
-;; (->> large
-;;      frequencies
-;;      vals
-;;      (apply -)
-;;      )
+(->> large
+     frequencies
+     vals
+     butlast
+    (apply - )
+     )
 ;; uses frequencies idiom to count the parenthesis...
 ;; pulls out just the values (vals)
 ;; the apply function does the math, but it returns the wrong floor
 ;; because frequencies also found a newline in my input, so I ended
 ;; up with 1 less than the correct answer.
+;; `butlast` removes the newline...as long as it is the last element returned
+;; from frequencies
 ;; Still, a cool implementation.
 
 
@@ -43,7 +46,7 @@
 
 ;; Execute part2 by passing it the first index, 0.   => (part2 0)
 ;; Check if the sum is negative; if it is, return the current index.
-;; If it is not negative yet, loop again, incrementing the index, 
+;; If it is not negative yet, loop again, incrementing the index,
 ;; and adding the current up/down to the sum
 (defn part2 [n]
   (loop [idx n sum 0]
@@ -51,6 +54,4 @@
         (recur (inc idx) (+ sum (updown (nth large idx)))))))
 
 
-(part2 0)   ;; => 1797 
-
-
+(part2 0)   ;; => 1797
