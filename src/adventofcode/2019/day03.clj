08d674c3d2607bb5352dcd49c39548e67dcf6e3a
Todd Smith <tmsmith328@gmail.com>
2022-01-05 00:32:16 -0700

    working day 3

--- src/adventofcode/2019/day03.clj
+++ src/adventofcode/2019/day03.clj
@@ -6,11 +6,16 @@
 (def input-file (str "src/adventofcode/2019/day" day "-input.txt"))
 
 (def small "R8,U5,L5,D3\nU7,R6,D4,L4")
+(def small2 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+(def small3 "R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83")
 
 (def large (slurp input-file))
 
 ;(def active small)
 (def active large)
+;(def active small3)
 
 (defn split [regex s]
   (string/split s regex))
@@ -72,10 +77,16 @@
   (let [x 
         (partition 2 2
                    (flatten
-                    (list (build-path (first lines)) (build-path (last lines)))))]
+                    (list
+                     (distinct (partition 2 2 (flatten (build-path (first lines)))))
+                     (distinct (partition 2 2 (flatten (build-path (last lines))))))))]
     (filter (fn [[k v]] (> v 1)) (frequencies x))))
 
-(sort (keys intersections))
+
+
+
+(defn man-dist [[x y]]
+  (+ (Math/abs x) (Math/abs y)))
 
 
 
@@ -85,7 +96,7 @@
 
 
 (defn part1 []
-
+  (first (sort (map #(man-dist %) (keys intersections))))
   )
 
 
@@ -101,10 +112,5 @@
 
 
 
-(def my-line [[5 10] [10 20]])
-
-(let [[[a b :as group1] [c d :as group2]] my-line]
-  (println a b group1)
-  (println c d group2))
 
 
